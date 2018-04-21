8839f110e08de1bf33df61fae1df17cb1bc9a644
Juliana Hosoume <ju.hosoume@gmail.com>
2018-04-20 22:35:41 -0300

    Finish chap 4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id]
-    @movie = Movie.find(id)
+    @movie = Movie.find params[:id]
   end
 
 
@@ -14,9 +13,36 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update 
+    @movie = Movie.find params[:id]
+    if @movie.update(movie_params)
+      flash[:notice] = "#{@movie.title} was successfullly updated!"
+      redirect_to movie_path(@movie)
+    end
+  end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted"
+    redirect_to movies_path
   end
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
+
 end
